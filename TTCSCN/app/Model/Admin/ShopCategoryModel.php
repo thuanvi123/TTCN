9da60b5e92515382a02bfcab0faaac6aab75ca1d
thuanvi123 <44500918+thuanvi123@users.noreply.github.com>
2019-12-12 16:36:35 +0700

    ddcx

---
+++ TTCSCN/app/Model/Admin/ShopCategoryModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShopCategoryModel extends Model
+{
+    //
+    protected $table = 'shop_category';
+    protected $guarded=[''];
+}
---
+++ TTCSCN/app/Model/Front/ShopCategoryModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Front;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShopCategoryModel extends Model
+{
+    protected $table = 'shop_category';
+}
